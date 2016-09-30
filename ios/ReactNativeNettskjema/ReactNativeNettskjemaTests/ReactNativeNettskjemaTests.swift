350668f833330a7a8a688cb062bd74fdb83b74ca
Jarle Fagerheim <jarle.fagerheim@usit.uio.no>
2016-09-30 10:38:33 +0200

    ios: bruk Swift 2.3

--- ios/ReactNativeNettskjema/ReactNativeNettskjemaTests/ReactNativeNettskjemaTests.swift
+++ ios/ReactNativeNettskjema/ReactNativeNettskjemaTests/ReactNativeNettskjemaTests.swift
@@ -28,9 +28,7 @@ class ReactNativeNettskjemaTests: XCTestCase {
     
     func testPerformanceExample() {
         // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+        XCTAssert(false)
     }
     
 }

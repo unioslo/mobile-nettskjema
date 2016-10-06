12eb46cdad33d001af0bc3b7c9c18a7dfadfde28
Jarle Fagerheim <jarle.fagerheim@usit.uio.no>
2016-10-06 14:52:34 +0200

    ios: rydd i testoppsettet

--- ios/ReactNativeNettskjema/ReactNativeNettskjemaTests/EncryptionTest.swift
+++ ios/ReactNativeNettskjema/ReactNativeNettskjemaTests/EncryptionTest.swift
@@ -4,34 +4,43 @@ import XCTest
 @testable import ReactNativeNettskjema
 
 private let STORAGE_DIRECTORY = DocumentStorageDirectory()
-private let ORIGINAL_FILE = STORAGE_DIRECTORY.newFileWithName("plaintext")
-private let ENCRYPTED_FILE = STORAGE_DIRECTORY.newFileWithName("plaintext.encrypted")
-private let DECRYPTED_FILE = STORAGE_DIRECTORY.newFileWithName("plaintext.decrypted")
-
+private let FILES = [
+    "original": STORAGE_DIRECTORY.newFileWithName("plaintext"),
+    "encrypted": STORAGE_DIRECTORY.newFileWithName("plaintext.encrypted"),
+    "decrypted": STORAGE_DIRECTORY.newFileWithName("plaintext.decrypted")
+]
 
 class EncryptionTest: XCTestCase {
     let key = String(count: 32, repeatedValue: Character("a"))
+    let fileManager = NSFileManager.defaultManager()
 
     override func setUp() {
         super.setUp()
-        let fileManager = NSFileManager.defaultManager()
-        fileManager.createFileAtPath(ORIGINAL_FILE.path!, contents: "1test\n2test\n3test".dataUsingEncoding(NSUTF8StringEncoding), attributes: nil)
+        fileManager.createFileAtPath(FILES["original"]!.path!, contents: "1test\n2test\n3test".dataUsingEncoding(NSUTF8StringEncoding), attributes: nil)
     }
-    
+ 
+    override func tearDown() {
+        super.tearDown()
+        for (_, url) in FILES {
+            do {
+                try fileManager.removeItemAtURL(url)
+            } catch { }
+        }
+    }
     
     func testDecryptedFileIsEqualToOriginal() {
         let method = ChaCha20Encryption(key: key)
-        try! EncryptionPipe(encryptionMethod: method).connect(ORIGINAL_FILE, outputFile: ENCRYPTED_FILE)
-        try! DecryptionPipe(encryptionMethod: method).connect(ENCRYPTED_FILE, outputFile: DECRYPTED_FILE)
+        try! EncryptionPipe(encryptionMethod: method).connect(FILES["original"]!, outputFile: FILES["encrypted"]!)
+        try! DecryptionPipe(encryptionMethod: method).connect(FILES["encrypted"]!, outputFile: FILES["decrypted"]!)
         
-        XCTAssertEqual(NSData(contentsOfURL: ORIGINAL_FILE), NSData(contentsOfURL: DECRYPTED_FILE))
-        XCTAssertNotEqual(NSData(contentsOfURL: ORIGINAL_FILE), NSData(contentsOfURL: ENCRYPTED_FILE))
+        XCTAssertEqual(NSData(contentsOfURL: FILES["original"]!), NSData(contentsOfURL: FILES["decrypted"]!))
+        XCTAssertNotEqual(NSData(contentsOfURL: FILES["original"]!), NSData(contentsOfURL: FILES["encrypted"]!))
     }
     
     func testAppendIvAndRetrieveFromCiphertext() {
         let encryptionMethod = ChaCha20Encryption(key: key)
-        try! EncryptionPipe(encryptionMethod: encryptionMethod).connect(ORIGINAL_FILE, outputFile: ENCRYPTED_FILE)
-        let retrievedIv = Base64IvAndCiphertext(data: NSData(contentsOfURL: ENCRYPTED_FILE)!).decodedIv
+        try! EncryptionPipe(encryptionMethod: encryptionMethod).connect(FILES["original"]!, outputFile: FILES["encrypted"]!)
+        let retrievedIv = Base64IvAndCiphertext(data: NSData(contentsOfURL: FILES["encrypted"]!)!).decodedIv
         XCTAssertEqual(retrievedIv.lengthOfBytesUsingEncoding(NSUTF8StringEncoding), 32)
     }
 }
